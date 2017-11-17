c4f3c973c5c497797c839f16a2eacedbc50224bb
Marcury <marcinpiechnik9@wp.pl>
2017-11-17 18:39:28 +0100

    Creating repository

---
+++ src/library/controller/LoginGuiController.java
@@ -0,0 +1,17 @@
+
+package library.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class LoginGuiController implements Initializable {
+
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }    
+    
+}
