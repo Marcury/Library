957b98cda38a423b7ca23d9b4b6ae2a375cc7126
Marcury <marcinpiechnik9@wp.pl>
2017-11-17 22:09:46 +0100

    Created: Login panel gui and controller Forget password panel gui and controller

--- src/library/controller/LoginGuiController.java
+++ src/library/controller/LoginGuiController.java
@@ -4,14 +4,72 @@ package library.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.stage.Stage;
 
+public class LoginGuiController implements Initializable{
 
-public class LoginGuiController implements Initializable {
+    
+    @FXML
+    private JFXButton loginButton;
 
-  
+    @FXML
+    private JFXTextField Login;
+
+    @FXML
+    private JFXPasswordField Passwd;
+
+    private String password; 
+    private String login; 
+    
+    
+
+    @FXML
+    void forgotPassword(ActionEvent event) throws IOException{
+            
+           Parent root = FXMLLoader.load(getClass().getResource("/libraryGui/ForgotPasswordGui.fxml"));
+            Scene scene = new Scene(root);
+            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.show();
+    }
+
+    @FXML
+    void login(ActionEvent event) throws IOException{
+        password = Passwd.getText();
+        login = Login.getText();
+        
+          if(login.equals("login") && password.equals("password") )
+          {
+              
+          }else {
+                    if(login.equals("login")== false)
+                    {
+                        Login.setText("Wrong Login");
+                        //Login.setBackground(Background.EMPTY);
+                    }
+                    if(password.equals("password") == false)
+                    {
+                        Passwd.setText("WRong Password");
+                       // Passwd.setBackground(Background.EMPTY);
+                    }
+              
+          }
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
        
-    }    
-    
+    }  
 }
---
+++ src/libraryGui/Login.java
@@ -0,0 +1,27 @@
+
+package libraryGui;
+
+import static javafx.application.Application.launch;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Login extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LoginGui.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
